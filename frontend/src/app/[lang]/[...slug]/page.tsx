d3a777bfcfe93702cdcd46a28dd0b0ac2b44977f
Eirikgrodal <eirik.grodal@gmail.com>
2024-01-22 16:53:10 +0100

    fikser error for deploying p[ vercel

--- frontend/src/app/[lang]/[...slug]/page.tsx
+++ frontend/src/app/[lang]/[...slug]/page.tsx
@@ -15,7 +15,7 @@ type Props = {
 export async function generateMetadata({params}: Props): Promise<Metadata> {
     const page = await getPageBySlug(params.slug, params.lang);
 
-    if (!page.data[0].attributes?.seo) return FALLBACK_SEO;
+    if (!page.data[0]?.attributes?.seo) return FALLBACK_SEO;
     const metadata = page.data[0].attributes.seo
 
     return {
@@ -24,10 +24,33 @@ export async function generateMetadata({params}: Props): Promise<Metadata> {
     }
 }
 
-
 export default async function PageRoute({params}: Props) {
     const page = await getPageBySlug(params.slug, params.lang);
-    if (page.data.length === 0) return null;
-    const contentSections = page.data[0].attributes.contentSections;
-    return contentSections.map((section: any, index: number) => sectionRenderer(section, index));
+    if (!page.data[0]) return null;
+    const contentSections = page.data[0]?.attributes?.contentSections;
+    return (
+        <>
+            {contentSections?.map((section: any, index: number) => sectionRenderer(section, index))}
+        </>
+    );
 }
+
+// export async function generateMetadata({params}: Props): Promise<Metadata> {
+//     const page = await getPageBySlug(params.slug, params.lang);
+
+//     if (!page.data[0].attributes?.seo) return FALLBACK_SEO;
+//     const metadata = page.data[0].attributes.seo
+
+//     return {
+//         title: metadata.metaTitle,
+//         description: metadata.metaDescription
+//     }
+// }
+
+
+// export default async function PageRoute({params}: Props) {
+//     const page = await getPageBySlug(params.slug, params.lang);
+//     if (page.data.length === 0) return null;
+//     const contentSections = page.data[0].attributes.contentSections;
+//     return contentSections.map((section: any, index: number) => sectionRenderer(section, index));
+// }
